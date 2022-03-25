667dc1a82c66ad2ed3193bd91f5c84f185345721
Aksel Borgen <akselborgen@outlook.com>
2022-03-25 17:22:58 +0100

    Time warp calculation

--- src/solution/routing.rs
+++ src/solution/routing.rs
@@ -1,3 +1,4 @@
+use std::cell::Cell;
 use std::{ops::Deref, sync::Arc};
 
 use pyo3::pyclass;
@@ -6,16 +7,18 @@ use crate::problem::Problem;
 use crate::solution::Visit;
 
 /// A plan is a series of visits over a planning period, often attributed to a single vessel.
+/// There are no restrictions on the ordering of the visits.
 #[pyclass]
 #[derive(Debug, Clone)]
 pub struct Plan {
-    /// The raw set of visits, setting no constraints on the ordering of them.
-    raw: Vec<Visit>,
+    /// The set of visits sorted by ascending time
+    sorted: Vec<Visit>,
 }
 
 impl Plan {
     fn new(raw: Vec<Visit>) -> Self {
-        Self { raw }
+        raw.sort_by_key(|v| v.time);
+        Self { sorted: raw }
     }
 }
 
@@ -23,7 +26,18 @@ impl Deref for Plan {
     type Target = [Visit];
 
     fn deref(&self) -> &Self::Target {
-        &self.raw
+        &self.sorted
+    }
+}
+
+impl<'s> IntoIterator for &'s Plan {
+    type Item = &'s Visit;
+
+    type IntoIter = <&'s [Visit] as IntoIterator>::IntoIter;
+
+    fn into_iter(self) -> Self::IntoIter {
+        let slice: &[Visit] = self;
+        slice.iter()
     }
 }
 
@@ -32,6 +46,8 @@ pub enum RoutingError {
 }
 
 /// A solution of the routing within a `Problem`, i.e. where and when each vessel arrives at different nodes throughout the planning period.
+/// It does not quarantee feasibility in any manner, it is possible to write a routing solution that e.g. requires a vessel to be present at two different nodes
+/// at the exact same time. Avoiding this is usually the responsibility of whoever creates the solution (e.g. the GA)
 #[pyclass]
 #[derive(Debug, Clone)]
 pub struct RoutingSolution {
@@ -39,6 +55,9 @@ pub struct RoutingSolution {
     problem: Arc<Problem>,
     /// The routes of each vessel.
     routes: Vec<Plan>,
+    /// The amount of time warp in this plan i.e. the sum of the number of time periods we would need to "missing timesteps"
+    /// between visits where it is physically impossible to travel between two nodes (only considering travel time; not loading time)
+    warp: Cell<Option<usize>>,
 }
 
 impl Deref for RoutingSolution {
@@ -50,14 +69,42 @@ impl Deref for RoutingSolution {
 }
 
 impl RoutingSolution {
-    pub fn new(problem: Arc<Problem>, routes: Vec<Vec<Visit>>) -> Result<Self, RoutingError> {
+    pub fn new(problem: Arc<Problem>, routes: Vec<Vec<Visit>>) -> Self {
+        // We won't bother returning a result from this, since it'll probably just be .unwrapped() anyways
         if routes.len() != problem.vessels().len() {
-            return Err(RoutingError::IncorrectVesselCount);
+            panic!("#r = {} != V = {}", routes.len(), problem.vessels().len());
         }
 
-        Ok(Self {
+        Self {
             problem,
             routes: routes.into_iter().map(|route| Plan::new(route)).collect(),
-        })
+            warp: Cell::default(),
+        }
+    }
+
+    pub fn problem(&self) -> &Problem {
+        &self.problem
+    }
+
+    fn update_warp(&self) -> usize {
+        let mut warp = 0;
+        for (v, route) in self.routes.iter().enumerate() {
+            let vessel = &self.problem.vessels()[v];
+            for visits in route.windows(2) {
+                let (v1, v2) = (visits[0], visits[1]);
+
+                let needed = self.problem.travel_time(v1.node, v2.node, vessel);
+                let available = v2.time - v1.time;
+
+                warp += std::cmp::max(needed - available, 0);
+            }
+        }
+
+        self.warp.set(Some(warp));
+        warp
+    }
+
+    pub fn warp(&self) -> usize {
+        self.warp.get().unwrap_or_else(|| self.update_warp())
     }
 }
