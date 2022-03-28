dcc35b9fc6880e893a606cb85510a6d12ed5ae34
Anders Bjelland <andertb@stud.ntnu.no>
2022-03-28 20:48:13 +0200

    trait for getting two visits as mutable in the same route

---
+++ src/utils.rs
@@ -0,0 +1,19 @@
+pub trait GetPairMut {
+    type Out;
+    fn get_pair_mut(self, v1: usize, v2: usize) -> (Self::Out, Self::Out);
+}
+
+impl<'a, T> GetPairMut for &'a mut [T] {
+    type Out = &'a mut T;
+
+    fn get_pair_mut(self, v1: usize, v2: usize) -> (Self::Out, Self::Out) {
+        assert!(v1 != v2);
+        let min = v1.min(v2);
+        let max = v2.max(v1);
+
+        let (one, rest) = self[min..].split_first_mut().unwrap();
+        let two = &mut rest[max - min - 1];
+
+        (one, two)
+    }
+}
