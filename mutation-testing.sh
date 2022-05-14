cd87a1f91d099f16eba439ac2f005e8fa15b57d8
Aksel Borgen <akselbor@ntnu.no>
2022-05-14 18:00:10 +0200

    Add stuff for testing parameters

---
+++ mutation-testing.sh
@@ -0,0 +1,9 @@
+# The baseline used by all
+baseline="add-random 0.01 ? remove-random 0.01 ? dedup"
+
+while read mutation; do
+	for p in 0.01 0.05 0.1 0.2 0.4
+	do
+		./target/release/master.exe --problem ~/master-playground/mirplib-rs/t45/LR2_22_DR3_333_VC4_V17a.json --log info --termination "1800 timeout 600 no-improvement |" rolling-horizon --population 10 --children 10 --subproblem-size 45 --mutation "$baseline $mutation $p ?" 2>> log.txt
+	done
+done < mutations.txt
