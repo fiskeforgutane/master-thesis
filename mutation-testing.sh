06a7c74789549f58be8c6967e1ce145f4646319c
Aksel Borgen <akselbor@ntnu.no>
2022-05-15 18:22:46 +0200

    Update mutation-testing.sh and mutations.txt for solstorm

--- mutation-testing.sh
+++ mutation-testing.sh
@@ -2,8 +2,8 @@
 baseline="add-random 0.01 ? remove-random 0.01 ? dedup"
 
 while read mutation; do
-	for p in 0.01 0.05 0.1 0.2 0.4
+	for p in 0.1
 	do
-		./target/release/master.exe --problem ~/master-playground/mirplib-rs/t45/LR2_22_DR3_333_VC4_V17a.json --log info --termination "1800 timeout 600 no-improvement |" rolling-horizon --population 10 --children 10 --subproblem-size 45 --mutation "$baseline $mutation $p ?" 2>> log.txt
+		./target/release/master.exe --problem ~/master-playground/mirplib-rs/t45/LR2_22_DR3_333_VC4_V17a.json --log info --termination "1800 timeout 600 no-improvement |" rolling-horizon --population 3 --children 3 --tournament 2 --subproblem-size 60 --mutation "$baseline $mutation $p ?" 2>> log.txt
 	done
 done < mutations.txt
