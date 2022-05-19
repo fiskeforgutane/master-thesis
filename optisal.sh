46e402861721bb30ce4968dbaf492927642a0bd3
Aksel Borgen <akselborgen@outlook.com>
2022-05-19 10:35:38 +0200

    File to run single-threaded benchmarks

---
+++ optisal.sh
@@ -0,0 +1,14 @@
+i=0
+pids=""
+for file in $(find ~/master-playground/mirplib-rs/t45/*.json)
+do
+    echo "Starting run on $file"
+    ~/master-thesis/target/release/master --problem file --log info --termination "7200 timeout 1800 no-improvement no-violation full-empty-valid & & |" rolling-horizon --threads 1 --full-penalty-after 3600000 --population 3 --children 3 --tournament 2 --subproblem-size 45 --mutation "std" 2>> log-$(hostname)-$(i).txt &
+    pids="$pids $!"
+    i=$((i + 1))
+
+    if [ $i -eq $1 ]; then
+        wait "$pid"
+        i=$((0))
+    fi
+done
