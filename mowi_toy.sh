6b9534e2c48ca162d8ff389c5ec6245f9b0e1c4d
Anders Tønsberg Bjelland <andertb@ntnu.no>
2022-05-20 21:23:56 +0200

    use several cores

--- mowi_toy.sh
+++ mowi_toy.sh
@@ -2,10 +2,10 @@ cd ~/master-thesis && cargo build --release;
 
 export i=0;
 export pids="";
-for file in $(find ~/master-playground/mowi-rs-red/toy/t$1/easy/*.json)
+for file in $(find ~/master-playground/mowi-rs-red/toy/t$1/middle/*.json)
 do
     echo "i=$i: Starting run on $file" >> log-$(hostname)-$i.txt
-    ~/master-thesis/target/release/master --problem $file --log info --termination "120 no-improvement" rolling-horizon --checkpoint-termination "7200 timeout 1800 no-improvement no-violation full-empty-valid & & |" --threads 1 --full-penalty-after 36000 --population 3 --children 3 --tournament 2 --subproblem-size 145 --step-length 2 --checkpoints $1 --mutation "std" --travel-at-cap 0 --travel-empty 0 2>> log-$(hostname)-$i.txt &
+    ~/master-thesis/target/release/master --problem $file --log info --termination "120 no-improvement" rolling-horizon --checkpoint-termination "7200 timeout 1800 no-improvement no-violation full-empty-valid & & |" --threads 8 --full-penalty-after 36000 --population 3 --children 3 --tournament 2 --subproblem-size 145 --step-length 2 --checkpoints $1 --mutation "std" --travel-at-cap 0 --travel-empty 0 2>> log-$(hostname)-$i.txt &
     pids="$pids $!"
     i=$((i + 1))
 
