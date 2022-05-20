691acebce7a1afce7c2bb632050f73f8205cf71b
Anders Tønsberg Bjelland <andertb@ntnu.no>
2022-05-20 10:14:35 +0200

    write problem being solved

--- mowi_gurobi.sh
+++ mowi_gurobi.sh
@@ -2,8 +2,8 @@ cd ~/master-thesis && cargo build --release;
 
 export i=0;
 export pids="";
-for file in $(find ~/master-playground/mowi-rs/toy/*/*/*.json)
+for file in $(find ~/master-playground/mowi-rs/toy/*/hard/*.json)
 do
-    echo "i=$i: Starting run on $file"
+    echo "i=$i: Starting run on $file" >> log-$(hostname)-$i.txt
     ~/master-thesis/target/release/master --problem $file --log info --termination "7200 timeout" exact >> log-$(hostname)-$i.txt
 done
