3501c32ab2b3b06436d5697fddf09c319d294ba4
Aksel Borgen <akselborgen@outlook.com>
2022-05-22 16:18:38 +0200

    Extend the time limit for solstorm-1t

--- solstorm-1t.sh
+++ solstorm-1t.sh
@@ -4,10 +4,11 @@ export pids="";
 for file in $(find ~/mirplib/t$1/*.json)
 do
     echo "i=$i: Starting run on $file"
-    ~/master-thesis/target/release/master --problem $file --log info --termination "180 no-improvement" rolling-horizon --checkpoints $1 --checkpoint-termination "7200 timeout 1800 no-improvement no-violation full-empty-valid & & |" --full-penalty-after 180000 --population 3 --children 3 --tournament 2 --step-length 2 --subproblem-size 45 --mutation "lite" --threads 1 2>> log-$(hostname)-$i.txt &
+    ~/master-thesis/target/release/master --problem $file --log info --termination "600 no-improvement" rolling-horizon --checkpoints $1 --checkpoint-termination "14400 timeout 1800 no-improvement no-violation full-empty-valid & & |" --full-penalty-after 180000 --population 3 --children 3 --tournament 2 --step-length 2 --subproblem-size 45 --mutation "lite" --threads 1 2>> log-$(hostname)-$i.txt &
+    
+    # Add the latest subprocess to the list of PIDs
     pids="$pids $!"
     i=$((i + 1))
-
     # Wait for all of the runs to complete before starting a new set.
     # Could've done this better
     if [ $i -eq $2 ]; then
